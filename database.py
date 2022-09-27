a943b8aa86a584f66fe88d70088a31b6eabc5716
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-09-27 17:15:21 +0900

    creating models

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from curses import echo
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 engine=create_engine("postgresl://postgres:6nhydvn2M@localhost/item_db",
@@ -7,3 +8,5 @@ engine=create_engine("postgresl://postgres:6nhydvn2M@localhost/item_db",
 )
 
 Base=declarative_base()
+
+SessionLocal=sessionmaker(bind=engine)
