e994ffa0fe84f7fdd55dc97d618c6b4a23079ed3
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-09-27 18:55:54 +0900

    finished setting up .env

--- database.py
+++ database.py
@@ -2,8 +2,12 @@ from curses import echo
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
-engine=create_engine("postgresql://postgres:6nhydvn2M@localhost/item_db",
+load_dotenv()
+
+engine=create_engine(f"postgresql://postgres:{str(os.getenv('POSTGRESQL_PASSWORD'))}@localhost/item_db",
     echo=True
 )
 
