2fa436ecc59a6d2b040007b400029bc31f603ec7
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-09-27 17:12:54 +0900

    setting up the db

--- database.py
+++ database.py
@@ -1,4 +1,9 @@
+from curses import echo
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 
-engine=create_engine()
+engine=create_engine("postgresl://postgres:6nhydvn2M@localhost/item_db",
+    echo=True
+)
+
+Base=declarative_base()
