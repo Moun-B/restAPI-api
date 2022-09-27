5ec5cbf6b011fb572255fce6a1df8fa53911e4d3
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-09-27 17:49:49 +0900

    setting up the model

--- models.py
+++ models.py
@@ -1,1 +1,9 @@
 from database import Base
+from sqlalchemy import String, Boolean, Integer, Text, Column
+
+class Item(Base):
+    __tablename__="items"
+    id=Column(Integer, primary_key=True)
+    name=Column(String(255), nullable=False, unique=True)
+    description=Column(Text)
+    on_offer=Column(Boolean, default=False)
